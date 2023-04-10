e5d736a4d462fa1da45bb01aac6d8c4b4ddf2ce7
Aleksey <medvedev_a_l@mail.ru>
2023-04-10 07:29:28 +0500

    Added textures

--- source/fragment.fs
+++ source/fragment.fs
@@ -1,9 +1,14 @@
 #version 330 core
 
-in vec3 colorOut;
+in vec3 Color;
+in vec2 TexCoord;
+
 out vec4 FragColor;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-	FragColor = vec4(colorOut, 1.0f);
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.3);
 };
--- source/main.cpp
+++ source/main.cpp
@@ -2,36 +2,28 @@
 #include <GLFW/glfw3.h>
 #include "shaderClass.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "../external/stb_image/stb_image.h"
+
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-float offsetX = 0.05f;
-float offsetY = 0.05f;
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 float vertices [] = {
-       -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-       -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
-        0.5f,  0.5f, 0.0f,  0.5f, 0.5f, 0.0f
-        };
-
-float vertices2Triangle [] = {
-        -0.85f,  0.65f, 0.f,
-        -0.61f,  0.65f, 0.f,
-        -0.73f,  1.0f,  0.f,
-
-        0.85f,  0.65f, 0.f,
-        0.61f,  0.65f, 0.f,
-        0.73f,  1.0f,  0.f
-        };
+       // positions          // colors           // texture coords
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
+    };
 
 unsigned int indices[] = {
-        0, 1, 2,
+        0, 1, 3,
         1, 2, 3
         };
 
@@ -68,7 +60,6 @@ int main()
     }
 
     Shader shader("..\\source\\vertex.vs", "..\\source\\fragment.fs");
-    Shader shaderTriangle("..\\source\\vertexYellow.vs", "..\\source\\fragmentYellow.fs");
 
     unsigned int EBO;
     glGenBuffers(1, &EBO);
@@ -83,30 +74,68 @@ int main()
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 *  sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 *  sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-    unsigned int VAOTriangle;
-    glGenVertexArrays(1, &VAOTriangle);
-    glBindVertexArray(VAOTriangle);
+    unsigned int texture1, texture2;
 
-    unsigned int VBOTriangle;
-    glGenBuffers(1, &VBOTriangle);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOTriangle);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2Triangle), vertices2Triangle, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 *  sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    std::string path = "..//resources//textures//container.jpg";
+    unsigned char* data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
+    if(data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture1" << std::endl;
+    }
 
-        // render loop
+    stbi_image_free(data);
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    path = "..//resources//textures//awesomeface.png";
+    data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
+    if(data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture2" << std::endl;
+    }
+
+    stbi_image_free(data);
+
+    shader.use();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
+
+    // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
     {
@@ -119,22 +148,16 @@ int main()
         glClearColor(0.1f, 0.0f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
 
         shader.use();
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, indices);
-        glBindVertexArray(0);
-
-
-        shaderTriangle.use();
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        glBindVertexArray(VAOTriangle);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        shaderTriangle.setFloat("offsetX", offsetX);
-        shaderTriangle.setFloat("offsetY", offsetY);
-        glDrawArrays(GL_TRIANGLES, 3, 3);
         glBindVertexArray(0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -144,10 +167,8 @@ int main()
     }
 
     glDeleteVertexArrays(1, &VAO);
-    glDeleteVertexArrays(1, &VAOTriangle);
     glDeleteBuffers(1, &EBO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &VBOTriangle);
 
     glfwTerminate();
     return 0;
@@ -157,42 +178,6 @@ void processInput(GLFWwindow* window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-
-    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-    {
-        offsetX += 0.01;
-        if(offsetX > 0.15)
-        {
-            offsetX = 0.15;
-        };
-    };
-
-    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-    {
-        offsetX -= 0.01;
-        if(offsetX < -1.3)
-        {
-            offsetX = -1.3;
-        };
-    };
-
-    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    {
-        offsetY += 0.01;
-        if(offsetY > 1.6)
-        {
-            offsetY = 1.6;
-        };
-    };
-
-    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    {
-        offsetY -= 0.01;
-        if(offsetY < 0)
-        {
-            offsetY = 0;
-        };
-    };
 };
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
