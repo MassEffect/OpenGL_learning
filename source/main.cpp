ab735c5ae2353746e198e0feb1c12e61e6cafb33
Aleksey <medvedev_a_l@mail.ru>
2023-04-27 04:22:38 +0500

    Camera

--- source/main.cpp
+++ source/main.cpp
@@ -20,6 +20,13 @@ const unsigned int SCR_HEIGHT = 600;
 
 float mixRatio = 0;
 
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  6.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
    float vertices[] = {
         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
          0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -180,10 +187,17 @@ int main()
     shader.setInt("texture1", 0);
     shader.setInt("texture2", 1);
 
+    glm::mat4 projection = glm::mat4(1.0f);
+    projection = glm::perspective(glm::radians(45.0f), (float)SCR_HEIGHT/(float)SCR_WIDTH, 0.1f, 100.0f);
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
     {
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         // input
         // ------
         processInput(window);
@@ -201,20 +215,15 @@ int main()
         glBindTexture(GL_TEXTURE_2D, texture2);
 
         shader.use();
-
-        //shader.setFloat("mixRatio", mixRatio);
+        glBindVertexArray(VAO);
 
         glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        glm::mat4 projection = glm::mat4(1.0f);
-        projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
-
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         int viewLoc = glGetUniformLocation(shader.ID, "view");
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
         int projectionLoc = glGetUniformLocation(shader.ID, "projection");
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-        glBindVertexArray(VAO);
 
         for(unsigned int i = 0; i < 10; i++)
         {
@@ -223,6 +232,7 @@ int main()
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i;
             model = glm::rotate(model, (i % 3) ? glm::radians(angle) : glm::radians((float)glfwGetTime() * 30.0f), glm::vec3(1.0f, 0.3f * i, 0.5f * i));
+
             mixRatio = i * 0.1f;
             shader.setFloat("mixRatio", mixRatio);
             int modelLoc = glGetUniformLocation(shader.ID, "model");
@@ -249,22 +259,22 @@ void processInput(GLFWwindow* window)
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-    if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    float cameraSpeed = static_cast<float>(deltaTime * 2.5);
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
     {
-        mixRatio = mixRatio + 0.01;
-        if(mixRatio > 1.0)
-        {
-            mixRatio = 1.0;
-        }
-    };
-
-    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
     {
-        mixRatio = mixRatio - 0.01;
-        if(mixRatio < 0)
-        {
-            mixRatio = 0;
-        }
+        cameraPos -= cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
     };
 };
 
