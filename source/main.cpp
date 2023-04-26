8bc62a3d19a6e7b8e5e7621bb7305f55668db809
Aleksey <medvedev_a_l@mail.ru>
2023-04-26 06:56:41 +0500

    Coordinate systems exersices

--- source/main.cpp
+++ source/main.cpp
@@ -123,7 +123,7 @@ int main()
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 *  sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -202,7 +202,7 @@ int main()
 
         shader.use();
 
-        shader.setFloat("mixRatio", mixRatio);
+        //shader.setFloat("mixRatio", mixRatio);
 
         glm::mat4 view = glm::mat4(1.0f);
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
@@ -218,10 +218,13 @@ int main()
 
         for(unsigned int i = 0; i < 10; i++)
         {
+            mixRatio = 0.0f;
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            model = glm::rotate(model, (i % 3) ? glm::radians(angle) : glm::radians((float)glfwGetTime() * 30.0f), glm::vec3(1.0f, 0.3f * i, 0.5f * i));
+            mixRatio = i * 0.1f;
+            shader.setFloat("mixRatio", mixRatio);
             int modelLoc = glGetUniformLocation(shader.ID, "model");
             glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
